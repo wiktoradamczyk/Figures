d0d1000e1616e0a5580e9b2eb79d69f737016a37
Adamczyk Wiktor <wiktor.adamczyk@microsoft.wsei.edu.pl>
2019-06-15 19:04:22 +0200

    Dodaj pliki projektów.

---
+++ FiguresLib/Figure.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace FiguresLib
+{
+    abstract public class Figure
+    {
+        public const int FRACTIONAL_DIGITS = 4;
+        public string Label { get; set; }
+
+        public Color Color { get; set; }
+
+        private static int counter = 0;
+        public Figure()
+        {
+            counter++;
+            this.Color = Color.Black;
+            Label = $"{GetType().Name} #{counter}";
+        }
+
+        abstract public void Draw();
+
+        public override string ToString() => $"Figure: {Label}";
+    }
+}
---
+++ FiguresLib/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+
+namespace FiguresLib
+{
+    public class Point : Figure, IEquatable<Point>
+    {
+        // fields
+        public readonly double X, Y; //immutability
+
+        // constructor
+        public Point(double x = 0.0, double y = 0.0)
+        {
+            X = Math.Round(x, Figure.FRACTIONAL_DIGITS);
+            Y = Math.Round(y, Figure.FRACTIONAL_DIGITS);
+            Color = Color.Blue;
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine($"Point({X}, {Y}), {Color}");
+        }
+
+        public bool Equals(Point other)
+        {
+           return this.X -- other.X && this.Y -- other.Y;
+        }
+    }
+
+
+
+
+
+
+}
